6afd53a0706017948966eadad19f69bca00ee935
Divino Schaeffer <divino.schaeffer02@gmail.com>
2024-03-22 14:59:51 +0100

    verification authentification user

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
+
+function isAuthentificate(request: NextRequest): boolean{
+    return request.cookies.has('token');
+}
+
+export function middleware(request: NextRequest){
+    if(request.nextUrl.pathname.startsWith('/dashboard')){
+        if(isAuthentificate(request)){
+            return NextResponse.redirect(new URL('/login', request.url));
+        }
+    }
+}
